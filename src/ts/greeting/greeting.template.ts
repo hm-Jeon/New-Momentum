4bc9334bbc6572570d7d012d562ce09648107fb0
hm-Jeon <jhm971102@gmail.com>
2022-04-27 21:50:28 +0900

    Greeting 기능 개발
    - constructor 수정
    - username 수정/삭제 기능 추가

--- src/ts/greeting/greeting.template.ts
+++ src/ts/greeting/greeting.template.ts
@@ -1,6 +1,11 @@
 const template = `
   {{#if hasData}}
-  <span>{{greeting_message}}</span>
+  <span>{{greetingMessage}} 
+    <span class="input" role="textbox" contenteditable spellcheck="false">
+      {{username}}
+    </span>
+    <i class="fa-solid fa-delete-left"></i>
+  </span>
   {{else}}
   <form>
     <input type="text" placeholder="Please input your name" />
--- src/ts/greeting/greeting.ts
+++ src/ts/greeting/greeting.ts
@@ -1,33 +1,60 @@
 import template from "./greeting.template";
 
+const USERNAME_KEY = "username";
+
 export default class Greeting {
   private container: HTMLElement;
 
   constructor(container: string) {
     this.container = document.querySelector(container) as HTMLElement;
-
     this.render();
-    (this.container.querySelector("form") as HTMLFormElement).addEventListener(
-      "submit",
-      this.inputName
-    );
   }
 
-  private inputName = (event: SubmitEvent) => {
+  private inputUsername = (event: SubmitEvent) => {
     event.preventDefault();
     const username: string = (this.container.querySelector("input") as HTMLInputElement).value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
 
     this.render();
   };
 
+  private deleteUsername = (event: Event) => {
+    localStorage.removeItem(USERNAME_KEY);
+
+    this.render();
+  };
+
+  private editUsername = (input: HTMLElement) => {
+    return (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        const newUsername: string = input.innerText;
+
+        localStorage.setItem(USERNAME_KEY, newUsername);
+        this.render();
+      }
+    };
+  };
+
   private render = () => {
-    const username = localStorage.getItem("username");
+    const username = localStorage.getItem(USERNAME_KEY);
     const hasData = !!username;
 
     this.container.innerHTML = template({
       hasData: hasData,
-      greeting_message: `Welcome! ${username}`,
+      greetingMessage: "Welcome!",
+      username: username,
     });
+
+    if (!hasData) {
+      const form: HTMLFormElement = this.container.querySelector("form") as HTMLFormElement;
+      form.addEventListener("submit", this.inputUsername);
+    } else {
+      const deleteBtn: HTMLElement = this.container.querySelector("i") as HTMLElement;
+      deleteBtn.addEventListener("click", this.deleteUsername);
+
+      const input: HTMLElement = this.container.querySelector(".input") as HTMLElement;
+      input.addEventListener("keypress", this.editUsername(input));
+    }
   };
 }
