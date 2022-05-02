102ff11b5a4d9939aaa82a3c32b2453ff8d1235a
hm-Jeon <jhm971102@gmail.com>
2022-05-02 19:45:07 +0900

    CSS 수정
    - 반응형 웹 구현 중

--- src/ts/weather/current_weather.template.ts
+++ src/ts/weather/current_weather.template.ts
@@ -4,9 +4,9 @@ const template = `
       <i class="{{icon}}"></i>
     </div>
     <div class="info">
-      <span class="city"><i class="fa-solid fa-location-dot"></i>{{city}}</span>
-      <span class="temp">{{temp}}</span>
-      <span class="status">{{status}}</span>
+    <span class="temp">{{temp}}</span>
+    <span class="status">{{status}}</span>
+    <span class="city"><i class="fa-solid fa-location-dot"></i>{{city}}</span>
     </div>
   </div>
 `;
